4367a1cf6f447e7147beec389e131ac1b37ee48c
shugo9538 <shugo9538@gmail.com>
2020-07-18 22:23:38 +0900

    first commit

---
+++ 5_1/ch05_test5.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+var host = '192.168.204.1';
+var port = 3000;
+
+server.listen(port, function() {
+  console.log(`웹 서버가 시작되었습니다. : %d`, port);
+});
+
+server.on('connection', function(socket) {
+	console.log('클라이언트가 접속했습니다. : %s, %d', socket.remoteAddress, socket.remotePort);
+});
+
+server.on('request', function(req, res) {
+	console.log('클라이언트 요청이 들어왔습니다.');
+
+  var filename = 'house.png';
+  var infile = fs.createReadStream(filename, {flags: 'r'} );
+  var filelength = 0;
+	var curlength = 0;
+
+  // infile.pipe(res);
+
+  fs.stat(filename, function(err, stats) {
+		filelength = stats.size;
+	});
+
+  res.writeHead(200, {"Content-Type": "image/png"});
+
+  infile.on('readable', function() {
+    var chunk;
+
+    while (null !== (chunk = infile.read())) {
+      console.log('읽어들인 데이터 크기 : %d 바이트', chunk.length);
+      curlength += chunk.length;
+      res.write(chunk, 'utf8', function(err) {
+    	console.log('파일 부분쓰기 완료 : %d, 파일 크기 : %d', curlength, filelength);
+    	if (curlength >= filelength) {
+        res.end();
+    	}
+    });
+	 }
+  });
+	// fs.readFile(filename, function(err, data) {
+	// 	res.writeHead(200, {"Content-Type": "image/png"});
+	// 	res.write(data);
+	// 	res.end();
+	// });
+});
+
+server.on('close', function() {
+	console.log('서버가 종료됩니다.');
+});
